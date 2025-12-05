21001beef9fa770b116237686181d9ba0d7d1bf3
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2025-12-05 16:15:19 -0500

    inicialización del proyecto

---
+++ demo/src/test/java/examenfinal/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package examenfinal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
